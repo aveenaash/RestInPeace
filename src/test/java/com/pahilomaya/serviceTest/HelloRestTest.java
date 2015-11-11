934c3ad6d38f2f75c8b5e1369818ca0ee5e72c67
Mac88509 <mac88509@C02LR584FD56.local>
2015-11-11 10:07:54 -0600

    ADDED failed unit test to check if jenkins build works

---
+++ src/test/java/com/pahilomaya/serviceTest/HelloRestTest.java
@@ -0,0 +1,23 @@
+package com.pahilomaya.serviceTest;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.pahilomaya.restService.HelloRest;
+
+public class HelloRestTest {
+
+    HelloRest helloRest;
+
+    @Before
+    public void setup() {
+        helloRest = new HelloRest();
+    }
+
+    @Test
+    public void testSayHello() {
+        assertEquals(helloRest.sayHello(), "Just ssss Want to say Hello");
+    }
+}
